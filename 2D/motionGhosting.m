fa89852cf39c8cec18e4107a77329d5dde1488a6
Mostafa-Ghazi <32398665+Mostafa-Ghazi@users.noreply.github.com>
2022-08-31 13:26:07 +0200

    Update motionGhosting.m

--- 2D/motionGhosting.m
+++ 2D/motionGhosting.m
@@ -11,7 +11,7 @@ function imageSlice = motionGhosting(imageSlice, alpha, numReps, p)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%  This code is an implementation of the algorithm published in:  %%%%%
 %%%%%                          FAST-AID Brain                         %%%%%
-%%%%%                https://arxiv.org/abs/XXXX.YYYYY                 %%%%%
+%%%%%                https://arxiv.org/abs/2208.14360                 %%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Random motion ghosting artefact generation in 2D images
