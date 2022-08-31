cf47f26a6c65b40bae21b16bcd732754c0db3b4c
Mostafa-Ghazi <32398665+Mostafa-Ghazi@users.noreply.github.com>
2022-08-31 13:25:34 +0200

    Update elasticDeform.m

--- 2D/elasticDeform.m
+++ 2D/elasticDeform.m
@@ -11,7 +11,7 @@ function imageSlice = elasticDeform(imageSlice, sigma, alpha, interMethod, extra
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%  This code is an implementation of the algorithm published in:  %%%%%
 %%%%%                          FAST-AID Brain                         %%%%%
-%%%%%                https://arxiv.org/abs/XXXX.YYYYY                 %%%%%
+%%%%%                https://arxiv.org/abs/2208.14360                 %%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Elastic deformation of 2D images using random displacement fields
