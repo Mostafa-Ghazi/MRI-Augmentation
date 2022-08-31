f0e80868a0cd498fa99b317d61c4ac923debcbb6
Mostafa-Ghazi <32398665+Mostafa-Ghazi@users.noreply.github.com>
2022-08-31 13:27:10 +0200

    Update gibbsRinging.m

--- 3D/gibbsRinging.m
+++ 3D/gibbsRinging.m
@@ -11,7 +11,7 @@ function imageVolume = gibbsRinging(imageVolume, numSample, p)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%  This code is an implementation of the algorithm published in:  %%%%%
 %%%%%                          FAST-AID Brain                         %%%%%
-%%%%%                https://arxiv.org/abs/XXXX.YYYYY                 %%%%%
+%%%%%                https://arxiv.org/abs/2208.14360                 %%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Random Gibbs ringing (oscillation) artefact generation in 3D images
