81d7a6aeb793949b1f58e6189690283ba2402bbf
Mostafa-Ghazi <32398665+Mostafa-Ghazi@users.noreply.github.com>
2022-08-31 13:26:55 +0200

    Update elasticDeform.m

--- 3D/elasticDeform.m
+++ 3D/elasticDeform.m
@@ -11,7 +11,7 @@ function imageVolume = elasticDeform(imageVolume, sigma, alpha, interMethod, ext
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%  This code is an implementation of the algorithm published in:  %%%%%
 %%%%%                          FAST-AID Brain                         %%%%%
-%%%%%                https://arxiv.org/abs/XXXX.YYYYY                 %%%%%
+%%%%%                https://arxiv.org/abs/2208.14360                 %%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Elastic deformation of 3D images using random displacement fields
